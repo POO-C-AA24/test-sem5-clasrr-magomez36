10561ae8068415db9ad41034a972d460bef7049e
Manuel Gomez <magomez36@utpl.edu.ec>
2024-05-08 16:12:56 -0500

    commit de Manuel Gomez

--- AppTerrenos/src/Terreno.java
+++ AppTerrenos/src/Terreno.java
@@ -4,4 +4,17 @@ public class Terreno{
     public double vmc;
     public double area;
     public double costoFinal;
+
+    public Terreno(double alto, double ancho, double vmc, double area, double costoFinal) {
+        this.alto = alto;
+        this.ancho = ancho;
+        this.vmc = vmc;
+        this.area = area;
+        this.costoFinal = costoFinal;
+    }
+
+    @Override
+    public String toString() {
+        return "Terreno{" + "alto=" + alto + ", ancho=" + ancho + ", vmc=" + vmc + ", area=" + area + ", costoFinal=" + costoFinal + '}';
+    }
 }
